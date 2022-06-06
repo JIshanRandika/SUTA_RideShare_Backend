ab961eded816f0f7b55bac26fa2c2de41bcbc3af
JIshanRandika <jishanrandika@gmail.com>
2022-06-06 23:48:45 +0530

    getDrives

--- routes/drives.router.js
+++ routes/drives.router.js
@@ -3,5 +3,6 @@ const drives = require("../controllers/drives.controller.js");
 module.exports = function(app) {
 
     app.post('/api/drive', drives.addADrive);
+    app.post('/api/getDrives', drives.drives);
 
 }
