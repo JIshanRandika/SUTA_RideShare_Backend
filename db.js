469bec3ef7a7ac40bf4eb9c103f9ba0206a1f1bf
JIshanRandika <jishanrandika@gmail.com>
2022-06-10 15:14:00 +0530

    update

--- db.js
+++ db.js
@@ -1,16 +1,28 @@
 const mongoose = require("mongoose");
+//
+// module.exports = () => {
+// 	const connectionParams = {
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	};
+// 	try {
+// 		mongoose.connect(process.env.DB, connectionParams);
+// 		console.log("Connected to database successfully");
+// 	} catch (error) {
+// 		console.log(error);
+// 		console.log("Could not connect database!");
+// 	}
+// };
 
 module.exports = () => {
-	const connectionParams = {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	};
-	try {
-		mongoose.connect(process.env.DB, connectionParams);
-		console.log("Connected to database successfully");
-	} catch (error) {
-		console.log(error);
-		console.log("Could not connect database!");
-	}
-};
+	// mongoose.connect('mongodb+srv://ishan:1998@cluster0.btqiz.mongodb.net/?retryWrites=true&w=majority')
 
+	mongoose.connect('mongodb+srv://ishan:1998@cluster0.btqiz.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+		.then(async () => {
+			console.log("Successfully connected to MongoDB.");
+
+		}).catch(err => {
+		console.log('Could not connect to MongoDB.');
+		process.exit();
+	});
+}
