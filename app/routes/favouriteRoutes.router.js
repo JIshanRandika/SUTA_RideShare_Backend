1c50c94db96bda4f5c3288aa85db523ef081f92e
JIshanRandika <jishanrandika@gmail.com>
2022-06-11 09:58:23 +0530

    update router

--- app/routes/favouriteRoutes.router.js
+++ app/routes/favouriteRoutes.router.js
@@ -2,7 +2,7 @@ const favoriteRoutes = require("../controllers/favoriteRoutes.controller");
 
 module.exports = function(app) {
 
-    app.addAFavouriteRoute('/api/drive', favoriteRoutes.addAFavouriteRoute);
+    app.addAFavouriteRoute('/api/addAFavouriteRoute', favoriteRoutes.addAFavouriteRoute);
 
 
 }
