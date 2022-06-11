ada725af5bd058d1e9dd6fcdec10d674ac14e759
JIshanRandika <jishanrandika@gmail.com>
2022-06-11 09:55:06 +0530

    favoriteRoutes controller

---
+++ app/controllers/favoriteRoutes.controller.js
@@ -0,0 +1,25 @@
+const FavouriteRoutes = require('../models/favoriteRoutes.model');
+
+//add a drive
+exports.addAFavouriteRoute = (req, res) => {
+    const favouriteRoute = new FavouriteRoutes({
+        originLongitude: req.body.originLongitude,
+        originLatitude: req.body.originLatitude,
+
+        destinationLongitude: req.body.destinationLongitude,
+        destinationLatitude: req.body.destinationLatitude,
+
+        email: req.body.email,
+
+    });
+    console.log('run')
+    // Save a order in the MongoDB
+    favouriteRoute.save().then(data => {
+        res.status(200).json(data);
+    }).catch(err => {
+        res.status(500).json({
+            message: "Fail!",
+            error: err.message
+        });
+    });
+};
