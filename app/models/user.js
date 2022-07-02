1455a4c8538f5010d075d04b486b0c0cf74edca6
JIshanRandika <jishanrandika@gmail.com>
2022-07-02 17:19:23 +0530

    groups

--- app/models/user.js
+++ app/models/user.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
 	name: { type: String, required: true },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
+	groupID: { type: String, required: true },
 	deviseToken: { type: String, required: true },
 });
 
@@ -25,6 +26,7 @@ const validate = (data) => {
 		name: Joi.string().required().label("Name"),
 		email: Joi.string().email().required().label("Email"),
 		password: passwordComplexity().required().label("Password"),
+		groupID: String,
 		deviseToken: String
 
 	});
